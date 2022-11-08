bede0f119d58513c571a713644299bcaa194538d
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-11-08 16:14:24 +0100

    tsconfig

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// let id = 5;
+// id = 'roman' //* error
+let id = 1;
+let id2 = 2;
+let id3 = 3;
+console.log(id3);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// let id = 5;
+// id = 'roman' //* error
+
+let id: number = 1;
+let id2: number = 2;
+let id3: number = 3;
+console.log(id3);
